dead9f0738232e0f7ef375ad572bec62cdeea570
Edgar Valli <edgarvalli80@gmail.com>
2024-06-05 16:43:11 -0600

    Se agrega bash script

---
+++ run_process.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+python3 visa.py
--- start_process_visa.py
+++ start_process_visa.py
@@ -1,11 +1,19 @@
 from time import sleep
-import subprocess, datetime
+import subprocess, datetime, os
 
 while True:
     d = datetime.datetime.now()
     if d.hour >= 19 or d.hour <= 3:
-        subprocess.run("python visa.py")
+        
+        cmd = 'python3 visa.py'
+        if os.name == 'nt':
+            cmd = 'python visa.py'
+        
+        subprocess.run(cmd)
     
+    elif d.hour == 3:
+        print('Cerrando el proceso....')
+        quit()    
     else:
         print('Aun no es horario de revisión de visas ....')
 
